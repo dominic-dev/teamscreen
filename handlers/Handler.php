b5c6b101f77942517e7fd056420549f637a34e82
dominic-wedesign <dominic@we-design.eu>
2018-03-21 12:42:45 +0100

    added abstract get

--- handlers/Handler.php
+++ handlers/Handler.php
@@ -2,11 +2,27 @@
 abstract class Handler {
     protected $dbh;
 
-    public function __construct(PDO $dbh){
+    public function __construct(PDO $dbh, string $table_name=null){
         $this->dbh = $dbh;
+
+        if (isset($tableName)){
+            $this->tableName = $tableName;
+        }
+        else {
+            $className = get_class($this);
+            $tableName= str_replace("Handler", "", $className);
+            $this->tableName = strtolower($tableName);
+        }
     }
 
-    // TODO get
+    public function get(int $id) {
+        $query = "select * from $this->tableName where id = :id";
+        $sth = $this->dbh->prepare($query);
+        $sth->bindParam('id', $id, PDO::PARAM_INT);
+        $sth->execute();
+        $result = $sth->fetch();
+        return $this->factory($result);
+    }
 
     abstract protected function factory(array $row);
 
