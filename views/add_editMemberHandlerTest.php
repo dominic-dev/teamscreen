6a99d442f51a47b36d23068ad6c8a53ca3f37b06
Unknown <vincent.huijts@gmail.com>
2018-03-21 16:04:57 +0100

    /TS-10: Auto stash before merge of "feature/TS-10" and "origin/feature/TS-10"
    basis for formhandler

--- views/add_editMemberHandlerTest.php
+++ views/add_editMemberHandlerTest.php
@@ -6,29 +6,62 @@
  * Time: 21:42
  */
 
+require_once('../models/Team.php');
+require_once('../models/Member.php');
 
 echo '<h3>DUMMY ADD/EDIT MEMBER HANDLER</h3>';
 
 echo '<h2>To test stuff coming from the addmember.html & editmember.html form</h2>';
 
-
-$membername = $_POST['membername'];
+$name = $_POST['membername'];
 $username = $_POST['username'];
-$team = $_POST['team'];
+$teamId = $_POST['team'];
+// klopt nog niet
 $drinkpreference = $_POST['drinkpreference'];
 $workingday = $_POST['workingday'];
+$destination = 'Dam 1, Amsterdam';
 
-echo "Teamlid naam: ". $membername . '<br>';
+$member = new Member();
+$member->setUsername($username);
+$member->setName($name);
+$member->setDestination($destination);
+$member->setDrinkpreference('koffie');
+
+function addMember($amember){
+
+    if(isset($amember)){
+        var_dump($amember);
+    }
+
+
+}
+
+
+echo "Teamlid naam: ". $name . '<br>';
 echo "JIRA gebruikersnaam: ". $username . '<br>';
-echo "Assigned aan team: ". $team . '<br>';
+echo "Assigned aan team: ". $teamId . '<br>';
 echo "Drankvoorkeur: ". $drinkpreference . '<br>';
 
 echo '<br>';
+
 
 echo "<strong>Werkdagen : </strong>" . '<br>';
 foreach($workingday as $item) {
     echo $item. '<br>';
 }
+
+addMember($member);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
