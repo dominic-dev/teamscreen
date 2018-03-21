d80e6e7ab9c140e19235b05659e67b52dcfe0ce2
CBoom <cboom.trf@gmail.com>
2018-03-21 12:14:12 +0100

    Carina:
    MemberHandler has factory method

--- handlers/MemberHandler.php
+++ handlers/MemberHandler.php
@@ -1,12 +1,14 @@
 <?php
 require_once('Handler.php');
+require_once('../models/Member.php');
+
 class MemberHandler extends Handler {
 
     /** Get by ID */
     public function get(int $id)
     {
         //Extend PDO connection with prepared statement
-        $querySql = "SELECT * from member where id = :id";
+        $querySql = "SELECT * from member where idTeamMember = :id";
         $statementHandler = $this->dbHandler->prepare($querySql);
         //Bind parameters
         $statementHandler->bindParam('id', $id, PDO::PARAM_INT);
@@ -14,6 +16,12 @@ class MemberHandler extends Handler {
         //Catch result by fetch
         $result = $statementHandler->fetch();
         return $result;
+    }
+
+    /** Parse a row into an object a factory */
+    protected function factory(array $row) : Member {
+        $member = new Member($row['idTeamMember'], $row['name'], $row['username'], $row['destination'], $row['drink_preferences'], $row['workdays']);
+        return $member;
     }
 
 }
