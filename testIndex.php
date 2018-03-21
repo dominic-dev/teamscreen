74b103f2938283b1d5d5fc3fbbaec3dac590cdd5
CBoom <cboom.trf@gmail.com>
2018-03-21 13:40:20 +0100

    Carina: Moved contents of index to testIndex.

---
+++ testIndex.php
@@ -0,0 +1,28 @@
+<?php
+require_once('models/Team.php');
+require_once('models/Member.php');
+require_once('handlers/Database.php');
+require_once('handlers/TeamHandler.php');
+require_once('handlers/MemberHandler.php');
+
+$team = new Team(5, "Pwap");
+$member = new Member(1, 'user01', 'klaas', $team);
+// var_dump($team);
+// var_dump($member);
+
+$db = new Database();
+$dbh = $db->getConnection();
+
+$th = new TeamHandler($dbh);
+$mh = new MemberHandler($dbh);
+
+$team = $th->get(1);
+$list = $th->getAll();
+var_dump($team);
+var_dump($list);
+
+$member1 = $mh->get(1);
+var_dump($member1);
+
+$memberArray = $mh->getAll();
+var_dump($memberArray);
