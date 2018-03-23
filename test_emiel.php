b7437dbedde7e40c967ecb2a4abb4be0b36a4adc
Emiel <emielnijhuis@gmail.com>
2018-03-23 13:13:23 +0100

    /TS-10: Auto stash before merge of "feature/TS-10" and "origin/feature/TS-10"
    Edited test file

--- test_emiel.php
+++ test_emiel.php
@@ -22,10 +22,25 @@ $member = new Member(1, 'user01', 'klaas', $team);
 // echo '<br/><br/>';
 // print_r($team);
 
-$workingdays = array("Monday", "Tuesday", "Wednesday", "Thursday");
 
-echo '<strong>Werkdagen:</strong><br>';
-echo implode(SEPARATOR, $workingdays);
+echo implode(SEPARATOR, array("Monday", "Tuesday", "Wednesday", "Thursday"));
+
+echo '<p/>';
+
+$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+
+//var_dump(json_decode($json));
+//var_dump(json_decode($json, true));
+
+$array = json_decode( $json, true );
+var_dump($array);
+
+foreach($array as $item) {
+    //echo $item['filename'];
+    echo $item.'<br/>';
+    echo $item['a'];
+}
+
 
 ?>
 
