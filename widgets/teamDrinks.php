31df9b2578c273218bb3a2e6ea46ebca4bf0c8c8
Emiel <emielnijhuis@gmail.com>
2018-03-23 15:07:27 +0100

    /TS-10: Auto stash before merge of "feature/TS-10" and "origin/feature/TS-10"
    Edit mockup TeamDrinks widget

--- widgets/teamDrinks.php
+++ widgets/teamDrinks.php
@@ -2,7 +2,12 @@
     <h2>Tijd voor koffie!</h2>
 
     <p/>
-    <table border="0" cellpadding="8" >
+    <table border="0" cellpadding="5" >
+
+        <tr>
+            <td colspan="1""><img height="60" src="http://tim.mybit.nl/jiraproxy.php/secure/useravatar?ownerId=agung.udijana" ></td>
+            <td colspan="2"><b>Agung</b>, het is jouw beurt om koffie te halen voor:</td>
+        </tr>
 
         <?php
 
@@ -14,7 +19,7 @@
 
             echo "<tr>";
 
-            echo "<td><img height=\"30\" src=\"http://tim.mybit.nl/jiraproxy.php/secure/useravatar?ownerId=$username\" ></td>";
+            echo "<td><img height=\"25\" src=\"http://tim.mybit.nl/jiraproxy.php/secure/useravatar?ownerId=$username\" ></td>";
             echo "<td>" . $member->getName() . "</td>";
             echo "<td>" . $member->getDrinkPreference(). "</td>";
 
