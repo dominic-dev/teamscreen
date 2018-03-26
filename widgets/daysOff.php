24c6febb407c330e909617d1e7c45665e72f7e00
Unknown <vincent.huijts@gmail.com>
2018-03-26 16:21:34 +0200

    /TS-18: Auto stash before merge of "feature/TS-18" and "origin/feature/TS-18"
    formatting widget

--- widgets/daysOff.php
+++ widgets/daysOff.php
@@ -1,21 +1,35 @@
 <?php
 
+
+$tabMembers = $allMembers;
+
+
+
+
 ?>
 
-    <div id="daysOff" class="widgetBoxSmall">
+<div id="daysOff" class="widgetBoxSmall">
     <h2><span>&#127958 Vrije dagen</span>
-        <span class="weekButton">Huidige Week</span>
-        <span class="weekButton">Volgende week</span>
+        <div class="tab">
+            <button class="tablinks" onclick="<?php currentWeekMembers()  ?>; <script> window.location.reload(); </script>">Huidige week</button>
+            <button class="tablinks" onclick="<?php nextWeekMembers()  ?>; ">Volgende week</button>
+        </div>
     </h2>
+
     <div id="timeOff-list">
-        <?php foreach ($allMembers as $member): ?>
-        <div>
+        <?php foreach ($tabMembers as $member): ?>
+        <div class="timeOff-item">
             <span><img src="https://jira.local.mybit.nl/secure/useravatar?size=small&ownerId=<?= $member->getUsername(); ?>"/></span>
-            <span><?= $member->getName(); ?></span>
-            <span>&#128197 05-03-2018 &#128336 vanaf 12:30</span>
+            <ul>
+                <li><span><?= $member->getName(); ?></span></li>
+                <li><span>&#128197 05-03-2018 &#128336 vanaf 12:30</span></li>
+            </ul>
         </div>
         <?php endforeach; ?>
 
     </div>
+
+
+
 
 </div>
