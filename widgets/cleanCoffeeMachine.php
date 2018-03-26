64d6e627b481c62574903d92ccc5d5bc8c395763
paul <paulsinterniklaas@hotmail.com>
2018-03-26 12:12:14 +0200

    /TS-10: Auto stash before merge of "feature/TS-10" and "origin/feature/TS-10"
    eerste opzet schoonaken widget

--- widgets/cleanCoffeeMachine.php
+++ widgets/cleanCoffeeMachine.php
@@ -1,10 +1,25 @@
+<?php
+
+$randomNumber = array_rand($allMembers,1);
+$cleaner = ($allMembers[$randomNumber]);
+
+?>
 <div id="cleanCoffeeMachine" class="widgetBoxSmall">
     <h2>Schoonmaken koffie</h2>
-    <?php
+
+    <div id="cleanerAvatar">
+
+        <?=$cleaner->getName()?>
+
+    </div>
 
 
+    <div id="txt">
+        <strong><?=$cleaner->getName()?>,</strong> jij gaat vandaag het koffieapparaat schoonmaken
 
-    ?>
+    </div>
+
+
 
 </div>
 
