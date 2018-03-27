6477d3ea9d5077699a76317479965e6bc70a4b91
paul <paulsinterniklaas@hotmail.com>
2018-03-27 15:55:41 +0200

    TS-16: Het werkt nu beter

--- widgets/cleanCoffeeMachine.php
+++ widgets/cleanCoffeeMachine.php
@@ -13,41 +13,33 @@
 
 
     <?php
-    const CLEAN_COFFEE_REFRESH = 60*60*24;
+    const CLEAN_COFFEE_REFRESH = 60 * 60 * 24;
 
-    $refresh = true;
-    if (isset($_SESSION['timeCleanCoffeeMachine'])) {
-        //24 hr refresh
-        $refresh = (time() - $_SESSION['timeCleanCoffeeMachine']) >= CLEAN_COFFEE_REFRESH;
-    }
+    if (empty($presentCoffeeMachineUsers)) {
+        echo '<div id="cleanerTxt">Er is op dit moment niemand beschikbaar.</div>';
+    } else {
+        $refresh = true;
+        if (isset($_SESSION['timeCleanCoffeeMachine'])) {
+            //24 hr refresh
+            $refresh = (time() - $_SESSION['timeCleanCoffeeMachine']) >= CLEAN_COFFEE_REFRESH;
+        }
 
-    if ($refresh) {
-        if (empty($presentCoffeeMachineUsers)) {
-            echo '<div id="cleanerTxt">Er is op dit moment niemand beschikbaar.</div>';
-        } else {
-
-            //picks a random index of the allMembers list
+        if ($refresh) {
+            //picks a random member of the presentCoffeeMachineUsers list
             $randomIndex = array_rand($presentCoffeeMachineUsers, 1);
+            $randomMember = $presentCoffeeMachineUsers[$randomIndex];
+            $randomMemberId = $randomMember->getId;
             // TODO use member id
-            $_SESSION['coffeeCleanerId'] = $randomIndex;
+            $_SESSION['coffeeCleanerId'] = $randomMemberId;
             $_SESSION['timeCleanCoffeeMachine'] = time();
         }
-//picks the Member-object that belongs to the random index
-    }
         $cleaner = ($presentCoffeeMachineUsers[$_SESSION['coffeeCleanerId']]);
-
         ?>
-
-
         <div id="cleanerAvatar">
-
             <img src="http://tim.mybit.nl/jiraproxy.php/secure/useravatar?ownerId=<?= $cleaner->getUsername() ?>"/>
-
         </div>
-
         <div id="cleanerTxt">
-
             <span class="fat"><?= $cleaner->getName() ?></span>, jij gaat vandaag het koffieapparaat schoonmaken!
-
         </div>
+    <?php } ?>
 </div>
