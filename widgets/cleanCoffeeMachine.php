9f4cd936da8d1f21040d6302c12db0b58a36d7d4
Unknown <dominic@we-design.eu>
2018-03-27 14:42:35 +0200

    todo use member id

--- widgets/cleanCoffeeMachine.php
+++ widgets/cleanCoffeeMachine.php
@@ -5,47 +5,49 @@
     @authors: Paul
 -->
 
-<?php
-
-$refresh = false;
-
-if(isset($_SESSION['timeCleanCoffeeMachine'])){
-    //24 hr refresh
-    $refresh = (time() - $_SESSION['timeCleanCoffeeMachine']) >= 86400;
-}
-else{
-    $_SESSION['timeCleanCoffeeMachine'] = time();
-    $refresh = true;
-}
-
-if($refresh){
-    //picks a random index of the allMembers list
-    $randomIndex = array_rand($presentCoffeeMachineUsers,1);
-    $_SESSION['indexMember'] = $randomIndex;
-    $_SESSION['timeCleanCoffeeMachine'] = time();
-}
-
-//picks the Member-object that belongs to the random index
-$cleaner = ($presentCoffeeMachineUsers[$_SESSION['indexMember']]);
-
-?>
-
 <link rel="stylesheet" href="widgets/cleanCoffeeMachine.css">
 
 <div id="cleanCoffeeMachine" class="widgetBoxSmall">
     <h2><img src="widgets/coffee.png">
         Koffieapparaat schoonmaken</h2>
 
-    <div id="cleanerAvatar">
 
-        <img src="http://tim.mybit.nl/jiraproxy.php/secure/useravatar?ownerId=<?= $cleaner->getUsername() ?>" />
+    <?php
+    const CLEAN_COFFEE_REFRESH = 60*60*24;
 
-    </div>
+    $refresh = true;
+    if (isset($_SESSION['timeCleanCoffeeMachine'])) {
+        //24 hr refresh
+        $refresh = (time() - $_SESSION['timeCleanCoffeeMachine']) >= CLEAN_COFFEE_REFRESH;
+    }
 
-    <div id="cleanerTxt">
+    if ($refresh) {
+        if (empty($presentCoffeeMachineUsers)) {
+            echo '<div id="cleanerTxt">Er is op dit moment niemand beschikbaar.</div>';
+        } else {
 
-        <span class="fat"><?=$cleaner->getName()?></span>, jij gaat vandaag het koffieapparaat schoonmaken!
+            //picks a random index of the allMembers list
+            $randomIndex = array_rand($presentCoffeeMachineUsers, 1);
+            // TODO use member id
+            $_SESSION['coffeeCleanerId'] = $randomIndex;
+            $_SESSION['timeCleanCoffeeMachine'] = time();
+        }
+//picks the Member-object that belongs to the random index
+    }
+        $cleaner = ($presentCoffeeMachineUsers[$_SESSION['coffeeCleanerId']]);
 
-    </div>
+        ?>
+
+
+        <div id="cleanerAvatar">
+
+            <img src="http://tim.mybit.nl/jiraproxy.php/secure/useravatar?ownerId=<?= $cleaner->getUsername() ?>"/>
+
+        </div>
+
+        <div id="cleanerTxt">
+
+            <span class="fat"><?= $cleaner->getName() ?></span>, jij gaat vandaag het koffieapparaat schoonmaken!
+
+        </div>
 </div>
-
