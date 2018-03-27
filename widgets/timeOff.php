833a4cba29481ebc57bfd01a8cbf0812b85c91aa
Vincent <vincent.huijts@gmail.com>
2018-03-27 15:44:55 +0200

    finished with present queries

--- widgets/timeOff.php
+++ widgets/timeOff.php
@@ -1,7 +1,37 @@
 <link rel="stylesheet" href="widgets/timeOff.css">
 
 <?php
-    $tabMembers = $teamMembers;
+
+$timeOffItems = array_merge($timeOffThisWeek, $timeOffNextWeek);
+
+$islandIcon = '&#127958';
+$calendarIcon = '&#128197';
+$clockIcon = '&#128336';
+$workDayEnd = 17;
+
+// returns different formatted strings based on start and enddate of leave
+// multiple days = startdate t/m enddate
+// single day = startdate
+// single day back before end = enddate + time
+function dateString($startDate, $endDate) {
+
+    global $workDayEnd, $clockIcon;
+
+    $sameDay = date_format($startDate, 'd-m') === date_format($endDate, 'd-m');
+    $beforeEnd = ((int)date_format($endDate, 'H')) < $workDayEnd;
+
+    $result = null;
+
+    if($sameDay && $beforeEnd){
+        $result = date_format($endDate, 'd-m') . ' ' . $clockIcon . ' ' . date_format($endDate, 'H:i');
+    } elseif ($sameDay){
+        $result =  date_format($endDate, 'd-m');
+    } else {
+        $result =  date_format($startDate, 'd-m') . ' t/m ' . date_format($endDate, 'd-m');
+
+    }
+    return $result;
+}
 ?>
 
 <<<<<<< development:widgets/daysOff.php
@@ -9,21 +39,22 @@
     <h2>&#127958 Vrije dagen</h2>
 =======
 <div id="timeOff" class="widgetBoxSmall">
-    <h2>&#127958 Vrije dagen</h2>
+    <h2><?= $islandIcon; ?> Vrije dagen</h2>
 
     <div id="timeOff-list">
-        <?php foreach ($tabMembers as $member): ?>
+        <?php foreach ($timeOffItems as $unit) : ?>
         <div class="timeOff-item">
             <span class="timeOff-box" "timeOff-avatar">
-                <img class="userimg" src="http://tim.mybit.nl/jiraproxy.php/secure/useravatar?size=small&ownerId=<?= $member->getUsername(); ?>"/>
+                 <img class="userimg" src="http://tim.mybit.nl/jiraproxy.php/secure/useravatar?size=small&ownerId=<?= $unit->getMember()->getUsername(); ?>"/>
             </span>
             <span class="timeOff-box" "memberInfo">
-                <span class="timeOff-name"><?= $member->getName(); ?></span>
-                <span class="timeOff-date">&#128197 05-03-2018 &#128336 vanaf 12:30</span>
+                <span class="timeOff-name"><?= $unit->getMember()->getName(); ?></span>
+            <span class="timeOff-date">
+                <?= $calendarIcon; ?>
+                <?= dateString(new DateTime($unit->getStartTime()),new DateTime($unit->getEndTime())); ?> </span>
             </span>
         </div>
         <?php endforeach; ?>
-
     </div>
 
 
