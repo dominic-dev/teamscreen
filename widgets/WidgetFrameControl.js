10d484488844ab6f3d9b229720e189c90a0bc7ab
Unknown <dominic@we-design.eu>
2018-03-28 14:44:37 +0200

    made scroll function applicable to scrollable

--- widgets/WidgetFrameControl.js
+++ widgets/WidgetFrameControl.js
@@ -3,16 +3,16 @@ const FRAMES_PER_SECOND = 20;
 const FRAME_PAUSE_TIME_IN_MS = 5000;
 
 $(document).ready(function(){
-    // Cache drinkList
-    var drinkList = $('#drink-list');
-
-    // Calculate distance to scroll
-    var scrollDistance = drinkList[0].scrollHeight - drinkList.height();
-
-    // Set timer
-    window.setInterval(function(){
-        var target = drinkList.scrollTop() == 0 ? scrollDistance : 0;
-        drinkList.animate({scrollTop : target}, 1000);
-    }, FRAME_PAUSE_TIME_IN_MS);
-
+    // For each scrollable
+    $('.scrollable').each(function(){
+        // Cache this
+        $this = $(this);
+        // Calculate scroll distance
+        $this.scrollDistance = $this[0].scrollHeight - $this.height();
+        // Set timer
+        window.setInterval(function(){
+            var target = $this.scrollTop() == 0 ? $this.scrollDistance : 0;
+            $this.animate({scrollTop : target}, 1000);
+        }, FRAME_PAUSE_TIME_IN_MS);
+    });
 });
