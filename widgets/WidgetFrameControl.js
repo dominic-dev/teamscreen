bb3c646bed8be2e8f8a2a7996f7f45c47a8029fc
dominic-wedesign <dominic@we-design.eu>
2018-03-28 14:27:37 +0200

    switch to jquery, fix CPU sink

--- widgets/WidgetFrameControl.js
+++ widgets/WidgetFrameControl.js
@@ -1,35 +1,18 @@
+/* @author Dominic */
+const FRAMES_PER_SECOND = 20;
+const FRAME_PAUSE_TIME_IN_MS = 5000;
 
+$(document).ready(function(){
+    // Cache drinkList
+    var drinkList = $('#drink-list');
 
-/**
- * Animate the list if the list is longer than the defined widget height.
- * @type {HTMLElement | null}
- */
-var drinklist = document.getElementById('drink-list');
-var FRAMES_PER_SECOND = 20;
-var FRAME_PAUSE_TIME_IN_MS = 5000;
+    // Calculate distance to scroll
+    var scrollDistance = drinkList[0].scrollHeight - drinkList.height();
 
-/**
- * Pauses widget frame when it reaches bottom and top of the frame.
- * @param milliseconds
- */
-function sleep(milliseconds) {
-    var currentTime = new Date().getTime();
-    while (currentTime + milliseconds >= new Date().getTime()) {
-    }
-}
-function step() {
-    console.log(drinklist.scrollTop + 2);
-    console.log((drinklist.scrollHeight - drinklist.offsetHeight));
-    if (drinklist.scrollTop <= 0) {
-        direction = 1;
-        sleep(FRAME_PAUSE_TIME_IN_MS);
-    } else if (drinklist.scrollTop + 2 > ((drinklist.scrollHeight - drinklist.offsetHeight))) {
-        direction = -1;
-        sleep(FRAME_PAUSE_TIME_IN_MS);
-    }
-    drinklist.scrollTop += direction;
-    setTimeout(function () {
-        window.requestAnimationFrame(step);
-    }, 1000 / FRAMES_PER_SECOND);
-}
-window.requestAnimationFrame(step);
+    // Set timer
+    window.setInterval(function(){
+        var target = drinkList.scrollTop() == 0 ? scrollDistance : 0;
+        drinkList.animate({scrollTop : target}, 1000);
+    }, FRAME_PAUSE_TIME_IN_MS);
+
+});
