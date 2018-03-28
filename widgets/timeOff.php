812132ae027d7557737316e8fa3c0fd33cc78220
Vincent <vincent.huijts@gmail.com>
2018-03-28 09:43:42 +0200

    improvements

--- widgets/timeOff.php
+++ widgets/timeOff.php
@@ -8,22 +8,27 @@ $islandIcon = '&#127958';
 $calendarIcon = '&#128197';
 $clockIcon = '&#128336';
 $workDayEnd = 17;
+$workDayStart = 9;
 
 // returns different formatted strings based on start and enddate of leave
 // multiple days = startdate t/m enddate
 // single day = startdate
 // single day back before end = enddate + time
+// not yet implemented: arriving later, arriving later and return before day end
 function dateString($startDate, $endDate) {
 
-    global $workDayEnd, $clockIcon;
+    global $workDayEnd, $workDayStart, $clockIcon;
 
     $sameDay = date_format($startDate, 'd-m') === date_format($endDate, 'd-m');
-    $beforeEnd = ((int)date_format($endDate, 'H')) < $workDayEnd;
+    $goneBeforeEnd = ((int)date_format($startDate, 'H')) > $workDayStart && ((int)date_format($endDate, 'H')) > $workDayEnd;
+    //not yet implemented
+    $startLater = ((int)date_format($startDate, 'H')) < $workDayStart && ((int)date_format($endDate, 'H')) < $workDayEnd;;
+    $goneAfterStartBackBeforeEnd = ((int)date_format($startDate, 'H')) > $workDayStart && ((int)date_format($endDate, 'H')) < $workDayEnd;;
 
     $result = null;
 
-    if($sameDay && $beforeEnd){
-        $result = date_format($endDate, 'd-m') . ' ' . $clockIcon . ' ' . date_format($endDate, 'H:i');
+    if($sameDay && $goneBeforeEnd){
+        $result = date_format($startDate, 'd-m') . ' ' . $clockIcon . ' vanaf ' . date_format($startDate, 'H:i');
     } elseif ($sameDay){
         $result =  date_format($endDate, 'd-m');
     } else {
